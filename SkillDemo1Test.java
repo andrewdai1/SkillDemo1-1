c98bfd67d45d07c0425e27176d79e7c028a956d8
andrewdai1 <97704775+andrewdai1@users.noreply.github.com>
2022-02-04 16:43:40 -0800

    Update SkillDemo1Test.java

--- SkillDemo1Test.java
+++ SkillDemo1Test.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemo1Test {
     @Test
     public void test() {
-        assertEquals(3, SkillDemo1.multiply(2, 1)); //fail
+        assertEquals(2, SkillDemo1.multiply(2, 1)); //not fail
     }
 }
