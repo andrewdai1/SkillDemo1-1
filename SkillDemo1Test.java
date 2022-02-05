bee20614c8705fc2de3d96347382c9fc8bcafb99
andrewdai1 <97704775+andrewdai1@users.noreply.github.com>
2022-02-04 16:42:38 -0800

    a

---
+++ SkillDemo1Test.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemo1Test {
+    @Test
+    public void test() {
+        assertEquals(3, SkillDemo1.multiply(2, 1)); //fail
+    }
+}
